19c4aa111b72d769a44857343317b7dcd08f763b
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-10-08 13:32:21 +0100

    wrong version

--- prerequisites.ps1
+++ prerequisites.ps1
@@ -8,11 +8,11 @@ $build = [System.Environment]::OSVersion.version.Build
 $virtu = Get-ComputerInfo -property "HyperVRequirementVirtualizationFirmwareEnabled"
 $virtu = $virtu.HyperVRequirementVirtualizationFirmwareEnabled
 
-if(($version -eq 10) -and ($build -ge 18632) -and ($virtu -eq $true)){
+if(($version -eq 10) -and ($build -ge 18362) -and ($virtu -eq $true)){
     Write-Host "Your system is ready! You can start the setup 😀" -ForegroundColor Green
 }elseif($version -ne 10){
     Write-Host "Please update to Windows 10 before going any further." -ForegroundColor Yellow
-}elseif($build -lt 18632){
+}elseif($build -lt 18362){
     Write-Host "You need to install the latest updates before going any further." -ForegroundColor Yellow
 }elseif($virtu -eq $false){
     Write-Host "You need to enable virtualization in the BIOS." -ForegroundColor Yellow
