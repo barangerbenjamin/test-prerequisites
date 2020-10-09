726971357986f3caa2da5c76ae3305c4fa357ae0
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-10-09 09:57:33 +0100

    add auto zoom install

---
+++ utilities.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "Installing Zoom, it will autostart at the end"
+
+msiexec /package ZoomInstallerFull.msi /lex zoommsi.log ZoomAutoStart="true"
+Start-Sleep -s 5
+Start-Process -FilePath "C:\Program Files (x86)\Zoom\bin\zoom.exe"
