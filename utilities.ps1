712c5a81f16c6b813a50c56474b7ba93be180579
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-10-09 10:08:17 +0100

    add teamviewer

--- utilities.ps1
+++ utilities.ps1
@@ -1,5 +1,7 @@
 ﻿Write-Host "Installing Zoom, it will autostart at the end"
-
 msiexec /package ZoomInstallerFull.msi /lex zoommsi.log ZoomAutoStart="true"
 Start-Sleep -s 5
 Start-Process -FilePath "C:\Program Files (x86)\Zoom\bin\zoom.exe"
+
+Write-Host "Installing Zoom, it will autostart at the end"
+Invoke-WebRequest -Uri "https://download.teamviewer.com/download/TeamViewer_Setup.exe" -OutFile teamviewer.exe; .\teamviewer.exe
